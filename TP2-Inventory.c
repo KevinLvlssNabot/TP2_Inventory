ca459770187868b079095a07504127484446bc68
KevinLvlssNabot <59653290+KevinLvlssNabot@users.noreply.github.com>
2020-01-27 11:39:24 +0100

    Create TP2-Inventory.c

---
+++ TP2-Inventory.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Items_t items;
+
+struct Items {
+  char nom;
+  int prix;
+  int quatite;
+};
+
+
+
+
+
+int main() {
+
+  items epee = {"Epee", 50, 10};
+  items bouclier = {"Bouclier", 75, 10};
+  items chapeau = {"Chapeau", 100, 10};
+  items armure = {"Armure", 150, 10};
+  items sandales = {"Sandales", 200, 10};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  return 0;
+}
