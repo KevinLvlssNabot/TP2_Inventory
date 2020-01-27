425089ff16134363e49839d9bff002fd69ca0d5c
KevinLvlssNabot <59653290+KevinLvlssNabot@users.noreply.github.com>
2020-01-27 12:27:41 +0100

    Ajout visuel shop.

--- TP2-Inventory.c
+++ TP2-Inventory.c
@@ -4,13 +4,20 @@
 
 typedef struct Items_t items;
 
-struct Items {
-  char nom;
+struct Items_t{
+  char* nom;
   int prix;
-  int quatite;
+  int quantite;
 };
 
-
+//typedef struct Inventaires_t inventaires;
+//
+//struct Inventaires_t{
+//  char* items;
+//  int prix;
+//  int quantite;
+//
+//};
 
 
 
@@ -22,9 +29,40 @@ int main() {
   items armure = {"Armure", 150, 10};
   items sandales = {"Sandales", 200, 10};
 
+  items shop[5];
+  shop[0] = epee;
+  shop[1] = bouclier;
+  shop[2] = chapeau;
+  shop[3] = armure;
+  shop[4] = sandales;
 
 
+  char choix[10];
+          printf("      Nom     |   Prix (mambas d'or)   |   Quantites   |\n");
+          printf("--------------------------------------------------------------------\n");
+          printf("   %s       |            %d          |     x%d       |\n", shop[0]);
+          printf("   %s   |            %d          |     x%d       |\n", shop[1]);
+          printf("   %s    |            %d         |     x%d       |\n", shop[2]);
+          printf("   %s     |            %d         |     x%d       |\n", shop[3]);
+          printf("   %s   |            %d         |     x%d       |\n", shop[4]);
 
+
+      printf("Que voulez-vous acheter ?\n");
+        scanf("%s", &choix);
+            if (choix == "epee") {
+              epee.quantite = epee.quantite-1;
+            }
+
+            printf("      Nom     |   Prix (mambas d'or)   |   Quantites   |\n");
+            printf("--------------------------------------------------------------------\n");
+            printf("   %s       |            %d          |     x%d       |\n", shop[0]);
+            printf("   %s   |            %d          |     x%d       |\n", shop[1]);
+            printf("   %s    |            %d         |     x%d       |\n", shop[2]);
+            printf("   %s     |            %d         |     x%d       |\n", shop[3]);
+            printf("   %s   |            %d         |     x%d       |\n", shop[4]);
+printf("%d\n", epee.quantite);
+
+  items inventaires[20];
 
 
 
