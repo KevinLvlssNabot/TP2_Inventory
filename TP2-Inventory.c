dd3635dd40fe583a1bb0d92364fb841a1eaa1d87
NabotKevin <59653290+KevinLvlssNabot@users.noreply.github.com>
2020-01-28 22:10:11 +0100

    Ajout creation objet ( prix et nombre )

--- TP2-Inventory.c
+++ TP2-Inventory.c
@@ -25,13 +25,6 @@ void Color(int t,int f)
     SetConsoleTextAttribute(H,f*16+t);
 }
 
-int compare (const void * a, const void * b)
-{
-  items * pA = (items *) a;
-  items * pB = (items *) b;
-
-  return (strcmp(pA->nom, pB->nom));
-}
 
 int main() {
 
@@ -40,39 +33,44 @@ int main() {
   items chapeau = {"Chapeau", 100, 10};
   items armure = {"Armure", 150, 10};
   items sandales = {"Sandales", 200, 10};
+  items objetCree = {NULL};
+  char* nomTemp;
 
 shop magasin = {NULL};
 
 
-magasin.items_list = malloc(5* sizeof(items*));
+magasin.items_list = malloc(6* sizeof(items*));
 
 magasin.items_list[0]= &epee;
 magasin.items_list[1]= &bouclier;
 magasin.items_list[2]= &chapeau;
 magasin.items_list[3]= &armure;
 magasin.items_list[4]= &sandales;
+magasin.items_list[5]= &objetCree;
 
-
-
+  int objetPerso = 0;
+  int choix;
+  int confirmation = 5;
   int continuer = 1;
   int mambas = 1000;
 
 while (mambas > epee.prix) {
-qsort (magasin.items_list, 5, sizeof(items), compare);
-  int choix;
+
+
   Color(15,0);        printf("-----------------------Marchand-------------------------     ");                                                                                                      Color(5,0);    printf("-----------------------Joueur-------------------------\n");
   Color(15,0);        printf("      Nom     |   Prix (mambas d'or)   |   Quantites   |     ");                                                                                                      Color(5,0);    printf("      Nom     |   Prix (mambas d'or)   |   Quantites   |\n");
   Color(15,0);        printf("--------------------------------------------------------     ");                                                                                                      Color(5,0);    printf("-------------------------------------------------------\n");
-  Color(15,0);        printf("   %s       |            %d          |     x%d       |     ", magasin.items_list[0]->nom, magasin.items_list[0]->prix, magasin.items_list[0]->quantite);              Color(5,0);    printf("   %s       |            %d          |     x%d       |\n", magasin.items_list[0]->nom, magasin.items_list[0]->prix, magasin.items_list[0]->quantiteJoueur);
-  Color(15,0);        printf("   %s   |            %d          |     x%d       |     ", magasin.items_list[1]->nom, magasin.items_list[1]->prix, magasin.items_list[1]->quantite);                  Color(5,0);    printf("   %s   |            %d          |     x%d       |\n", magasin.items_list[1]->nom, magasin.items_list[1]->prix, magasin.items_list[1]->quantiteJoueur);
-  Color(15,0);        printf("   %s    |            %d         |     x%d       |     ", magasin.items_list[2]->nom, magasin.items_list[2]->prix, magasin.items_list[2]->quantite);                  Color(5,0);    printf("   %s    |            %d         |     x%d       |\n", magasin.items_list[2]->nom, magasin.items_list[2]->prix, magasin.items_list[2]->quantiteJoueur);
-  Color(15,0);        printf("   %s     |            %d         |     x%d       |     ", magasin.items_list[3]->nom, magasin.items_list[3]->prix, magasin.items_list[3]->quantite);                 Color(5,0);    printf("   %s     |            %d         |     x%d       |\n", magasin.items_list[3]->nom, magasin.items_list[3]->prix, magasin.items_list[3]->quantiteJoueur);
-  Color(15,0);        printf("   %s   |            %d         |     x%d       |     ", magasin.items_list[4]->nom, magasin.items_list[4]->prix, magasin.items_list[4]->quantite);                   Color(5,0);    printf("   %s   |            %d         |     x%d       |\n", magasin.items_list[4]->nom, magasin.items_list[4]->prix, magasin.items_list[4]->quantiteJoueur);
-                                                                                                                                        printf("                                                                                                      Mambas: %d\n", mambas);
+  Color(15,0);        printf("   %s       |            %d          |     x%d       |     ", magasin.items_list[0]->nom, magasin.items_list[0]->prix, magasin.items_list[0]->quantite);              Color(5,0);  if (epee.quantite > 0) {  printf("   %s       |            %d          |     x%d       |\n", magasin.items_list[0]->nom, magasin.items_list[0]->prix, magasin.items_list[0]->quantiteJoueur);}
+  Color(15,0);        printf("   %s   |            %d          |     x%d       |     ", magasin.items_list[1]->nom, magasin.items_list[1]->prix, magasin.items_list[1]->quantite);                  Color(5,0);  if (bouclier.quantite > 0) {  printf("   %s   |            %d          |     x%d       |\n", magasin.items_list[1]->nom, magasin.items_list[1]->prix, magasin.items_list[1]->quantiteJoueur);}
+  Color(15,0);        printf("   %s    |            %d         |     x%d       |     ", magasin.items_list[2]->nom, magasin.items_list[2]->prix, magasin.items_list[2]->quantite);                  Color(5,0);  if (chapeau.quantite > 0) {  printf("   %s    |            %d         |     x%d       |\n", magasin.items_list[2]->nom, magasin.items_list[2]->prix, magasin.items_list[2]->quantiteJoueur);}
+  Color(15,0);        printf("   %s     |            %d         |     x%d       |     ", magasin.items_list[3]->nom, magasin.items_list[3]->prix, magasin.items_list[3]->quantite);                 Color(5,0);  if (armure.quantite > 0) {  printf("   %s     |            %d         |     x%d       |\n", magasin.items_list[3]->nom, magasin.items_list[3]->prix, magasin.items_list[3]->quantiteJoueur);}
+  Color(15,0);        printf("   %s   |            %d         |     x%d       |     ", magasin.items_list[4]->nom, magasin.items_list[4]->prix, magasin.items_list[4]->quantite);                   Color(5,0);  if (sandales.quantite > 0) {  printf("   %s   |            %d         |     x%d       |\n", magasin.items_list[4]->nom, magasin.items_list[4]->prix, magasin.items_list[4]->quantiteJoueur);}
+                      printf("   %s   |            %d         |     x%d       |\n", magasin.items_list[5]->nom, magasin.items_list[5]->prix, magasin.items_list[5]->quantite);                                                                                                                  printf("                                                                                                      Mambas: %d\n", mambas);
 Color(15,0);
 
+
       printf("Que voulez-vous acheter ?\n");
-      printf("0 pour une %s, 1 pour un %s, 2 pour un %s, 3 pour un %s, 4 pour une %s\n", magasin.items_list[0]->nom, magasin.items_list[1]->nom, magasin.items_list[2]->nom, magasin.items_list[3]->nom, magasin.items_list[4]->nom);
+      printf("0 pour une %s, 1 pour un %s, 2 pour un %s, 3 pour un %s, 4 pour une %s, 5 pour creer un objet.\n", magasin.items_list[0]->nom, magasin.items_list[1]->nom, magasin.items_list[2]->nom, magasin.items_list[3]->nom, magasin.items_list[4]->nom);
         scanf("%d", &choix);
 
               if (choix == 0) {
@@ -100,12 +98,28 @@ Color(15,0);
                 sandales.quantite = sandales.quantite-1;
                 sandales.quantiteJoueur = sandales.quantiteJoueur+1;
                 mambas = mambas - sandales.prix;
+              } else if (choix == 5) {
+                printf("Vous desirez creer un objet ? Cela vous coutera 500 mambas.\n");
+                printf("0 pour oui, 1 pour non.\n");
+                scanf("%d", &confirmation);
+                  if (confirmation == 0) {
+                      objetPerso = 1;
+                      mambas = mambas - 500;
+                      printf("Comment souhaitez-vous appeler votre objet ?\n");
+                    //  scanf("%s", &objetCree.nom);
+                      printf("Combien doit-il couter ?\n");
+                      scanf("%d", &objetCree.prix);
+                      printf("Combien voulez-vous en creer ?\n");
+                      scanf("%d", &objetCree.quantite);
+                  } else {
+                    continue;
+                  }
               }
 
-
+  }
 // system("cls");
 
-}
+
 
 
 printf("Desole, vous n'avez plus assez d'argent pour acheter quoi que ce soit.\n");
